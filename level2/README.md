7ff7c4edc83ca9371b69658abe22122b64c89bbc
Smx <83521274@qq.com>
2025-06-12 23:41:09 +0800

    init

--- level2/README.md
+++ level2/README.md
@@ -5,3 +5,22 @@
 前端页面（俗称网页）能否反馈ansible数据即可。例如后端控制执行ansible all -m ping会返回一定的数据，该数据在前端简单展示即可
 如果不使用docker部署，则不得分
 后端语言不做限制
+# 使用说明
+- 构建镜像
+```
+docker build -t ansible_web .
+```
+![构建镜像](1.png)
+# 运行镜像
+```
+docker run --name ansible_web -d -p "5000:5000"  ansible_web
+```
+![运行镜像](2.png)
+# 运行镜像
+```
+docker run --name ansible_web -d -p "5000:5000"  ansible_web
+```
+# 运行结果
+浏览器访问网页
+http://111.92.241.12:5000/exec?command=/usr/local/bin/ansible%20%20-i%20/home/project/hosts%20all%20-m%20ping
+![运行结果](3.png)
--- level2/code/app.py
+++ level2/code/app.py
@@ -1,11 +1,31 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
+import subprocess
+ 
+def run_cmd(command):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            output = result.stdout.strip()
+            print(output)
+        else:
+            output = result.stderr.strip()
+            print(output)
+    except Exception as e:
+        output = str(e)
+    return output
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/exec')
+def exec_command():
+    command=request.args.get('command')
+    result=run_cmd(command)
+    return result
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
